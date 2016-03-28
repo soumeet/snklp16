2f430f5055cd9fed54e93fb2e06a604e6bdd1374
soumeet <soumeet.basak@gmail.com>
2016-03-29 01:01:46 +0530

    commit_29032016_0057
    major updates

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -4,13 +4,13 @@ header("Content-type: application/json");
 //=======================================
 $admin_email = "info@snklp.com";
 $from_name   = "sankalp'16";
-/*
+
 $con = mysqli_connect('mysql.hostinger.in', 'u937298476_admin', 'Sankalp@2016', 'u937298476_snklp');
 $db = mysqli_select_db($con, 'u937298476_snklp');
-*/
+/*
 $con = mysqli_connect('localhost', 'root', '', 'u937298476_snklp');
 $db = mysqli_select_db($con, 'u937298476_snklp');
-
+*/
 if(isset($_POST['subscriberemail'])) {
 
 	$subscriber_email = strip_tags($_POST['subscriberemail']);
